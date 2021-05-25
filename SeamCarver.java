ba6ec50dbb4196c87e5d1e62dd24a6a4279f3ac3
Clos2024 <56045580+Clos2024@users.noreply.github.com>
2021-05-24 22:49:08 -0500

    coded calculate energy

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,13 +1,17 @@
 
+import java.awt.Color;
+
 import lib.Picture;
 
 public class SeamCarver {
 
 	Picture picture;
+	private double[][] energy;
+	private double maxEnergy = 1000;
    // create a seam carver object based on the given picture
    public SeamCarver(Picture picture)
    {
-	   
+	   this.picture = picture;
    }
 
    // current picture
@@ -31,7 +35,37 @@ public class SeamCarver {
    // energy of pixel at column x and row y
    public double energy(int x, int y)
    {
+	   //	a 3 by 4 image	//
+	   ///////////////////////
+	   //|(0,0)|(1,0)|(2,0)|//
+	   //|(0,1)|(1,1)|(2,1)|//
+	   //|(0,2)|(1,2)|(2,2)|//
+	   //|(0,3)|(1,3)|(2,3)|//
+	   ///////////////////////
 	   
+	   var pictureWidth = width();
+	   var pictureHeight = height();
+	   
+	   //Energy for border pixels should be maxed
+	   if(x == 0 || x == pictureWidth - 1 || y == 0 || y == pictureHeight - 1)
+	   {
+		   return maxEnergy;
+	   }
+	   
+	   //Energy for non border pixel
+	   
+	   double xDelta = energyHelper(picture.get(x - 1, y), picture.get(x + 1, y));
+	   double yDelta = energyHelper(picture.get(x, y - 1), picture.get(x, y + 1));
+	   
+	   return Math.sqrt(yDelta + yDelta);
+	   
+   }
+   private double energyHelper(Color a, Color b)
+   {
+	   int red = a.getRed() - b.getRed();
+	   int green = a.getGreen() - b.getGreen();
+	   int blue = a.getBlue() - b.getBlue();
+	   return (red*red) + (green*green) + (blue*blue);
    }
 
    // sequence of indices for horizontal seam

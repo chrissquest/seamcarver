ad37435a09381b60c029ac055787f51344942f90
Chriss <drago15243@gmail.com>
2021-05-27 15:30:55 -0500

    energy test

--- EnergyTest.java
+++ EnergyTest.java
@@ -1,10 +1,22 @@
 import lib.Picture;
+import lib.StdOut;
 
 public class EnergyTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Picture picture = new Picture("3x4.png");
+		StdOut.printf("Testing 3x4 Image.\n", picture.width(), picture.height());
 
+		SeamCarver sc = new SeamCarver(picture);
+
+		StdOut.printf("Comparing values.\n");
+
+		for (int row = 0; row < sc.height(); row++) {
+			for (int col = 0; col < sc.width(); col++)
+				StdOut.println("do test here");
+			StdOut.println();
+		}
+		StdOut.printf("Test complete/incomplete");
 	}
 
 }

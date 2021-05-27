fae703f1bf93feeffe74453b87a38d51dbca394c
Clos2024 <56045580+Clos2024@users.noreply.github.com>
2021-05-27 01:12:30 -0500

    implemented Dijkstras for vertical and horizontal

--- SeamCarver.java
+++ SeamCarver.java
@@ -2,17 +2,24 @@
 import lib.Picture;
 
 import java.awt.Color;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.*;
 
 public class SeamCarver {
 
 	Picture picture;
 	public double[][] energy;
 	private double maxEnergy = 1000;
+    private int[][] visited;
    // create a seam carver object based on the given picture
    public SeamCarver(Picture picture)
    {
 	   this.picture = picture;
-	   this.energy = new double [width()][height()];
+	   
+	   visited = new int[width()][height()];
+	   energy = new double [width()][height()];
+	   
 	   for (int row = 0; row < width(); row++)
            for (int col = 0; col < height(); col++)
         	   energy[row][col] = energy(row,col);
@@ -76,56 +83,190 @@ public class SeamCarver {
    // sequence of indices for horizontal seam
    public int[] findHorizontalSeam()
    {
-	   int[] seam = new int[height()];
-
-	   return seam;
+	   int[] seam = new int[width()]; //stores lowest energy seam
+	   
+       double[] distTo = new double[height()]; // stores the total energy of a pixels total path for all left origins.
+       double[] oldDistTo = new double[height()]; // stores the previous row of pixel energy values so we can update distTo.
+       
+       //Visit every pixel in the image row by row
+       for (int row = 0; row < width(); row++) {
+           for (int col = 0; col < height(); col++) {
+        	   dykstraHor(row, col, distTo, oldDistTo);
+           }
+           System.arraycopy(distTo, 0, oldDistTo, 0, height()); //after each row, store the distTo values to oldDistTo so we can update total distance -
+       }													//when going through the next row.
+       
+       double min = oldDistTo[0]; //Pick first path total distance to compare to all others.
+       int best = 0; // integer to keep track of which path is the smallest
+       
+       for (int index = 0; index < oldDistTo.length; index++) {
+           if (oldDistTo[index] < min) {
+               min = oldDistTo[index]; // if current distanced indexed is smaller make it the new minimum.
+               best = index; //store what index min value was found at
+           }
+       }
+//       System.out.println(min);
+       
+       seam[width() - 1] = best;
+       for (int i = width() - 2; i >= 0; i--) { // Back track and fill seam array with the path with least energy
+           seam[i] = visited[i+1][best];
+           best = visited[i+1][best];			//Set best to now be the point we were just at
+       }
+       return seam;
    }
 
    // sequence of indices for vertical seam
    public int[] findVerticalSeam()
    {
+	   int[] seam = new int[height()]; //stores lowest energy seam
+	   
+       double[] distTo = new double[width()]; // stores the total energy of a pixels total path for all top origins.
+       double[] oldDistTo = new double[width()]; // stores the previous row of pixel energy values so we can update distTo.
 
-       int[] seam = new int[width()];
-       int[] LEseam = new int[width()];
-       double LEtotal = Double.MAX_VALUE;
-
-       int LEpixel = 0;
-       // find lowest energy pixel
-       for (int x = 1; x < width() - 1; x++) {
-            LEpixel = x;
-            seam[0] = LEpixel - 1;
-            double totalEnergy = 1000;
-
-           // Calculate seam from LEpixel
-           for (int y = 1; y < height(); y++) {
-               // Middle pixel base
-               double lowestEnergy = energy[LEpixel][y];
-
-               // Compare left pixel
-               if(energy[LEpixel - 1][y] < lowestEnergy){
-                   lowestEnergy = energy[LEpixel - 1][y];
-                   LEpixel = LEpixel - 1;
-               }
-               if(energy[LEpixel + 1][y] < lowestEnergy){
-                   lowestEnergy = energy[LEpixel + 1][y];
-                   LEpixel = LEpixel + 1;
-               }
-
-               seam[y] = LEpixel;
-               totalEnergy += lowestEnergy;
+       //Visit every pixel in the image row by row
+       for (int col = 0; col < height(); col++) {
+           for (int row = 0; row < width(); row++) {
+        	   dykstraVert(row, col, distTo, oldDistTo);
            }
-
-           // if it's a lower energy seam, use it
-           if(totalEnergy < LEtotal) {
-               LEtotal = totalEnergy;
-               LEseam = seam;
+           System.arraycopy(distTo, 0, oldDistTo, 0, width()); //after each row, store the distTo values to oldDistTo so we can update total distance -
+       }													   //when going through the next row.
+       
+       
+       double min = oldDistTo[0]; //Pick first path total distance to compare to all others.
+       int best = 0; // integer to keep track of which path is the smallest
+       
+       //compare each distance to min
+       for (int index = 0; index < oldDistTo.length; index++) {
+           if (oldDistTo[index] < min) {
+               min = oldDistTo[index]; // if current distanced indexed is smaller make it the new minimum.
+               best = index; //store what index min value was found at
            }
-
        }
+      System.out.println(best);
 
-       return LEseam;
+       seam[height() - 1] = best;
+       for (int i = height() - 2; i >= 0; i--) { // Back track and fill seam array with the path with least energy
+           seam[i] = visited[best][i + 1];
+           best = visited[best][i + 1]; //Set best to now be the point we were just at
+       }
+       
+       
+       return seam;
    }
 
+   private void dykstraVert(int col, int row, double[] distTo, double[] oldDistTo) 
+   {
+	   //Dealing with top row of pixels
+	   if (row == 0) {
+           distTo[col] = maxEnergy;
+           visited[col][row] = -1;
+           return;
+       }
+	   
+	   //Dealing with left column of pixels
+       if (col == 0) {
+           double a = oldDistTo[col];
+           double b = oldDistTo[col + 1];
+           double min = Math.min(a, b);
+           distTo[col] = min + energy[col][row];
+           if (a < min) {
+        	   visited[col][row] = col;
+           } else {
+        	   visited[col][row] = col + 1;
+           }
+           return;
+       }
+       
+       //Dealing with right column of pixels
+       if (col == width() - 1) {
+           // we have only 2 edges
+           double a = oldDistTo[col];
+           double b = oldDistTo[col - 1];
+           double min = Math.min(a, b);
+           distTo[col] = min + energy[col][row];
+           if (a < min) {
+        	   visited[col][row] = col;
+           } else {
+        	   visited[col][row] = col - 1;
+           }
+           return;
+       }
+
+       //Dealing with the middle value of pixels
+       
+       // for 3 edges
+       double left = oldDistTo[col - 1];
+       double mid = oldDistTo[col];
+       double right = oldDistTo[col + 1];
+
+       double min = Math.min(Math.min(left, mid), right);
+
+       distTo[col] = min + energy[col][row];
+       if (min == left) {
+    	   visited[col][row] = col - 1;
+       } else if (min == mid) {
+    	   visited[col][row] = col;
+       } else {
+    	   visited[col][row] = col + 1;
+       }
+   }
+   
+   private void dykstraHor(int col, int row, double[] distTo, double[] oldDistTo) 
+   {
+	   
+	   //Dealing with left column of pixels
+       if (col == 0) {
+           distTo[row] = maxEnergy;
+           visited[col][row] = -1;
+           return;
+       }
+       
+	   //Dealing with top row of pixels
+	   if (row == 0) {
+           double a = oldDistTo[row];
+           double b = oldDistTo[row + 1];
+           double min = Math.min(a, b);
+           distTo[row] = min + energy[col][row];
+           if (a < min) {
+        	   visited[col][row] = row;
+           } else {
+        	   visited[col][row] = row + 1;
+           }
+           return;
+       }
+       
+       //Dealing with right column of pixels
+       if (row == height() - 1) {
+           // we have only 2 edges
+           double a = oldDistTo[row];
+           double b = oldDistTo[row - 1];
+           double min = Math.min(a, b);
+           distTo[row] = min + energy[col][row];
+           if (a < min) {
+        	   visited[col][row] = col;
+           } else {
+        	   visited[col][row] = col - 1;
+           }
+           return;
+       }
+
+       //Dealing with the middle value of pixels
+       // for 3 edges
+       double left = oldDistTo[row - 1];
+       double mid = oldDistTo[row];
+       double right = oldDistTo[row + 1];
+
+       double min = Math.min(Math.min(left, mid), right);
+
+       distTo[row] = min + energy[col][row];
+       if (min == left) {
+    	   visited[col][row] = row - 1;
+       } else if (min == mid) {
+    	   visited[col][row] = row;
+       } else {
+    	   visited[col][row] = row + 1;
+       }
+   }
    // remove horizontal seam from current picture
    public void removeHorizontalSeam(int[] seam)
    {
